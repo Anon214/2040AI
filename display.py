a217c0e7a1306bf6f7e0e92e5e4b346bfa9c887b
Anon214 <43688027+Anon214@users.noreply.github.com>
2021-06-06 12:22:00 -0400

    added inital 2 squares

--- display.py
+++ display.py
@@ -1,8 +1,10 @@
 from tkinter import *
-import Colors as c
+import colors as c
+import random
 
-tile_size = 120
-border_edge = 15
+border_edge = 10
+width = 700
+height = 700
 
 
 class Game(Frame):
@@ -11,37 +13,79 @@ class Game(Frame):
 
         self.grid()
         self.master.title('2048')
+        src_width = self.winfo_screenwidth()
+        src_height = self.winfo_screenheight()
+        x = (src_width / 2) - (width / 2)
+        y = (src_height / 2) - (height / 2)
+        self.master.geometry(f'{width}x{height}+{int(x)}+{int(y)}')
+        self.cells = []
         self.build_gui()
+        self.draw_initial_squares()
 
+        self.master.resizable(False, False)
         self.mainloop()
 
+    # builds base board
     def build_gui(self):
-        background = Frame(self, bg=c.BACKGROUND_COLOR)
-        background.grid()
+        background = Frame(self, bg=c.BACKGROUND_COLOR, width=width, height=height)
+        background.pack()
+
         game_background = Frame(background, bg=c.GAME_BACKGROUND_COLOR)
-        game_background.grid(padx=20, pady=20)
+        game_background.place(relwidth=0.8, relheight=0.8, relx=0.1, rely=0.09)
 
         row_num = 0
         col_num = 0
-        for x in range(1, 17):
-            pad_x_value = border_edge
-            pad_y_value = (0, border_edge)
+        tile_size = (width - (border_edge * 6)) / 5 - 0.75
 
-            if x > 4:
-                pad_x_value = (0, border_edge)
-
-            if x == 1 or x == 5 or x == 9 or x == 13:
+        for x in range(4):
+            row_insert = []
+            for y in range(4):
+                pad_x_value = border_edge
                 pad_y_value = border_edge
 
-            cell = Frame(game_background, bg=c.EMPTY_TILE_COLOR, width=tile_size, height=tile_size)
-            cell.grid(row=row_num, column=col_num, padx=pad_x_value, pady=pad_y_value)
-            row_num += 1
-            if row_num % 4 == 0:
-                col_num += 1
-                row_num = 0
+                if y > 0:
+                    pad_y_value = (0, border_edge)
 
-        cell1 = Frame(game_background, bg="blue", width=tile_size, height=tile_size)
-        cell.grid(row=1, column=1, padx=pad_x_value, pady=pad_y_value)
+                if x > 0:
+                    pad_x_value = (0, border_edge)
+
+                cell = Frame(game_background, bg=c.EMPTY_TILE_COLOR, width=tile_size, height=tile_size)
+                cell.grid(row=row_num, column=col_num, padx=pad_x_value, pady=pad_y_value)
+                cell_number = Label(game_background, bg=c.EMPTY_TILE_COLOR, justify=CENTER, text="")
+                cell_number.grid(row=row_num, column=col_num, padx=pad_x_value, pady=pad_y_value)
+                cell_data = {'frame': cell, 'number': cell_number}
+                row_insert.append(cell_data)
+
+                row_num += 1
+                if row_num % 4 == 0:
+                    col_num += 1
+                    row_num = 0
+            self.cells.append(row_insert)
+
+    def draw_initial_squares(self):
+        matrix = [[0] * 4 for x in range(4)]
+        for i in range(2):
+            if random.random() < 0.9:
+                value = 2
+            else:
+                value = 4
+            row = random.randint(0, 3)
+            col = random.randint(0, 3)
+
+            while matrix[row][col] != 0:
+                row = random.randint(0, 3)
+                col = random.randint(0, 3)
+
+            matrix[row][col] = value
+
+            self.cells[col][row]["frame"].configure(bg=c.TILE_COLORS[value])
+            self.cells[col][row]["number"].configure(bg=c.TILE_COLORS[value],
+                                                     text=str(value),
+                                                     font=c.LABEL_FONT,
+                                                     fg=c.LABEL_COLORS)
+
+        for elem in matrix:
+            print(elem)
 
 
 Game()
